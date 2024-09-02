22005cc07b218c92fd1c9c0d2ae0a3a10106783d
DjonHd <djonohara@gmail.com>
2024-09-02 16:59:41 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'module_1_3'
+print(name)
+name = 'Djon'
+print('Name: ' + name)
+age = 43
+print('Age: ' + str(age))
+new_age = age + 1
+print('New age: ' + str(new_age))
+is_student = True
+print('is_Student: ' + str(is_student))
+
+
